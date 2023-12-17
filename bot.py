20962dd3c6a4f1c00b9867fb1082150070197a81
Rplay <americarplay@gmail.com>
2023-12-17 23:05:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,18 +46,18 @@ class Bot(Client):
         app = web.AppRunner(web_app)
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
-        try:
-            await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
-        except:
-            print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
-            exit()
-        try:
-            m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
-            await m.delete()
-        except:
-            print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
-            exit()
-        print(f"\nPyrogram [v{__version__}] Bot [{username}] Started With Python [v{platform.python_version()}]\n")
+#        try:
+#            await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
+#        except:
+#            print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
+#            exit()
+#        try:
+#            m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
+#            await m.delete()
+#        except:
+#            print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
+#            exit()
+#        print(f"\nPyrogram [v{__version__}] Bot [{username}] Started With Python [v{platform.python_version()}]\n")
 
 
     async def stop(self, *args):
