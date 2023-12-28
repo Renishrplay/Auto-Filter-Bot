fb4f73eeda87b12d9267ea67aacba2c2be6c55fc
Rplay <americarplay@gmail.com>
2023-12-28 23:40:53 +0100

    Update info.py

--- info.py
+++ info.py
@@ -16,69 +16,32 @@ def is_valid_ip(ip):
 
 # Bot information
 API_ID = environ.get('API_ID', '')
-if len(API_ID) == 0:
-    print('Error - API_ID is missing, exiting now')
-    exit()
-else:
-    API_ID = int(API_ID)
 API_HASH = environ.get('API_HASH', '')
-if len(API_HASH) == 0:
-    print('Error - API_HASH is missing, exiting now')
-    exit()
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
-if len(BOT_TOKEN) == 0:
-    print('Error - BOT_TOKEN is missing, exiting now')
-    exit()
-PORT = int(environ.get('PORT', '80'))
+PORT = int(environ.get('PORT', '8080'))
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/1aabc32de4442dbc5a2a0.jpg https://graph.org/file/9458bd38633097021468e.jpg https://graph.org/file/b352b109ff6ab64a0dec5.jpg https://graph.org/file/1e58ec7e88373d43fe698.jpg https://graph.org/file/e29fe074270cda1833462.jpg https://graph.org/file/ce952c92d4c42fd588213.jpg https://graph.org/file/5ee19f028c4585af12b6c.jpg https://graph.org/file/41dfb4d472208520d0314.jpg https://graph.org/file/ca6de43379db0999546ad.jpg https://graph.org/file/2212ca8121a17edb5a2e7.jpg')).split()
 
 # Bot Admins
-ADMINS = environ.get('ADMINS', '')
-if len(ADMINS) == 0:
-    print('Error - ADMINS is missing, exiting now')
-    exit()
-else:
-    ADMINS = [int(admins) for admins in ADMINS.split()]
+ADMINS = environ.get('ADMINS', '1927155351')
 
 # Channels
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '').split()]
-#if len(INDEX_CHANNELS) == 0:
-#    print('Info - INDEX_CHANNELS is empty')
 AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '').split()]
-#if len(AUTH_CHANNEL) == 0:
-#    print('Info - AUTH_CHANNEL is empty')
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1001986722664')
-#if len(LOG_CHANNEL) == 0:
-#    print('Error - LOG_CHANNEL is missing, exiting now')
-#    exit()
-#else:
- #   LOG_CHANNEL = int(LOG_CHANNEL)
-    
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')
-#if len(SUPPORT_GROUP) == 0:
-#    print('Error - SUPPORT_GROUP is missing, exiting now')
-#    exit()
-#else:
-#    SUPPORT_GROUP = int(SUPPORT_GROUP)
-    
+LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1001986722664')    
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')    
 OPENAI_API = environ.get('OPENAI_API', '')
-#if len(OPENAI_API) == 0:
-#    print('Info - OPENAI_API is empty')
 
 # MongoDB information
 DATABASE_URL = environ.get('DATABASE_URL', "")
-if len(DATABASE_URL) == 0:
-    print('Error - DATABASE_URL is missing, exiting now')
-    exit()
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "ANUSHKA")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/SL_Bots_Support')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/SL_Bots_Updates')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/SL_Films_World')
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://telegram.dog/Technical_Discussion_Group')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://telegram.dog/ANUSHKA_SEN_CHANNEL')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://telegram.dog/ANUSHKA_SEN_GROUP')
 
 # Bot settings
 AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
@@ -86,8 +49,8 @@ IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SPELL_CHECK = is_enabled(environ.get("SPELL_CHECK", "True"), True)
 SHORTLINK = is_enabled((environ.get('SHORTLINK', "False")), False)
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "False")), False)
-WELCOME = is_enabled((environ.get('WELCOME', "False")), False)
+AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
+WELCOME = is_enabled((environ.get('WELCOME', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 LINK_MODE = is_enabled(environ.get("LINK_MODE", "True"), True)
@@ -103,27 +66,10 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "36f1ae74ba1aa01e5bd73bdd0bc22aa915
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = is_enabled(environ.get("IS_VERIFY", "False"), False)
 WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/SL_Bots_Updates")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/SL_Bots_Updates")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/jenna_movie/48")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/jenna_movie/48")
 INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
 
 # stream features vars
 BIN_CHANNEL = environ.get("BIN_CHANNEL", LOG_CHANNEL)
-if len(BIN_CHANNEL) == 0:
-    print('Error - BIN_CHANNEL is missing, exiting now')
-    exit()
-else:
-    BIN_CHANNEL = int(BIN_CHANNEL)
 URL = environ.get("URL", "")
-#if len(URL) == 0:
-#    print('Error - URL is missing, exiting now')
-#    exit()
-#else:
-#    if URL.startswith(('https://', 'http://')):
-#        if not URL.endswith("/"):
- #           URL += '/'
- #   elif is_valid_ip(URL):
-#        URL = f'http://{URL}/'
- #   else:
-#        print('Error - URL is not valid, exiting now')
-#        exit()
