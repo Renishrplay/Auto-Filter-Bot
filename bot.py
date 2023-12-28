156f7687f2761b06fa1cc24c559eeb367ac46071
Rplay <americarplay@gmail.com>
2023-12-29 00:06:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import pyromod
 from pyrogram import Client, __version__
 from database.ia_filterdb import Media
+from pyrogram.raw.all import layer
 from aiohttp import web
 from database.users_chats_db import db
 from web import web_app
@@ -8,9 +9,11 @@ from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from datetime import date, datetime
 import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 
+TIMEZONE = "Asia/Kolkata"
 
 class Bot(Client):
     def __init__(self):
@@ -43,22 +46,17 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         username = '@' + me.username
+        temp.B_MEN = me.mention
         app = web.AppRunner(web_app)
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
-#        try:
-#            await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
-#        except:
-#            print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
-#            exit()
-#        try:
-#            m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
-#            await m.delete()
-#        except:
-#            print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
-#            exit()
-#        print(f"\nPyrogram [v{__version__}] Bot [{username}] Started With Python [v{platform.python_version()}]\n")
-
+        await self.send_message(LOG_CHANNEL, text=f"<b>{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : <code>{today}</code>\n⏰ Tɪᴍᴇ : <code>{time}</code>\n🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>{TIMEZONE}</code>\n\n🉐 Vᴇʀsɪᴏɴ : <code>v{__version__} (Layer {layer})</code></b>")
+    #    await ai.send_message(LOG_CHANNEL, text=f"<b>{anushka.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : <code>{today}</code>\n⏰ Tɪᴍᴇ : <code>{time}</code>\n🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>{TIMEZONE}</code>\n\n🉐 Vᴇʀsɪᴏɴ : <code>v{__version__} (Layer {layer})</code></b>")
+        
 
     async def stop(self, *args):
         await super().stop()
@@ -66,29 +64,6 @@ class Bot(Client):
 
 
     async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                async for message in app.iter_messages("pyrogram", 1000, 100):
-                    print(message.text)
-        """
         current = offset
         while True:
             new_diff = min(200, limit - current)
